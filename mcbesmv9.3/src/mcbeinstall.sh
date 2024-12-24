df85a8e417257737a94aeaa265a20a5db9702dff
Joseph Vibhu <123543750+josephvibhu@users.noreply.github.com>
2024-12-24 19:50:54 +0530

    Update mcbeinstall.sh

--- mcbesmv9.3/src/mcbeinstall.sh
+++ mcbesmv9.3/src/mcbeinstall.sh
@@ -4,16 +4,16 @@
 # Function to install minecraft bedrock server
 mcbeinstall() {
     echo "Checking for the latest version of Minecraft Bedrock server available..."
-    VERSION_FILE="server/version.txt"
+    VERSION_FILE="$SCRIPT_DIR/server/version.txt"
 
     # Set random number for user-agent
     RandNum=$RANDOM
 
     # Download the latest version of Minecraft Bedrock server page
-    curl -H "Accept-Encoding: identity" -H "Accept-Language: en" -L -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.33 (KHTML, like Gecko) Chrome/90.0.$RandNum.212 Safari/537.33" -o temp/version.html https://minecraft.net/en-us/download/server/bedrock/
+    curl -H "Accept-Encoding: identity" -H "Accept-Language: en" -L -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.33 (KHTML, like Gecko) Chrome/90.0.$RandNum.212 Safari/537.33" -o $SCRIPT_DIR/temp/version.html https://minecraft.net/en-us/download/server/bedrock/
 
     # Extract the download URL for the latest server version
-    LATEST_VERSION=$(basename $(grep -o 'https://www.minecraft.net/bedrockdedicatedserver/bin-linux/[^"]*' temp/version.html))  # Get the latest version file name
+    LATEST_VERSION=$(basename $(grep -o 'https://www.minecraft.net/bedrockdedicatedserver/bin-linux/[^"]*' $SCRIPT_DIR/temp/version.html))  # Get the latest version file name
 
     # Construct the download URL
     DownloadURL="https://www.minecraft.net/bedrockdedicatedserver/bin-linux/$LATEST_VERSION"
@@ -24,20 +24,17 @@ mcbeinstall() {
     # Download the latest version of Minecraft Bedrock dedicated server
     echo "Downloading the latest version of Minecraft Bedrock server..."
     UserName=$(whoami)
-    curl -H "Accept-Encoding: identity" -H "Accept-Language: en" -L -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.33 (KHTML, like Gecko) Chrome/90.0.$RandNum.212 Safari/537.33" -o "temp/$LATEST_VERSION" "$DownloadURL"
+    curl -H "Accept-Encoding: identity" -H "Accept-Language: en" -L -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.33 (KHTML, like Gecko) Chrome/90.0.$RandNum.212 Safari/537.33" -o "$SCRIPT_DIR/temp/$LATEST_VERSION" "$DownloadURL"
 
     # Unzip the downloaded file
     echo "Unzipping the downloaded file..."
-    unzip -o -q "temp/$LATEST_VERSION" -d temp/bedrock-server
+    unzip -o -q "$SCRIPT_DIR/temp/$LATEST_VERSION" -d $SCRIPT_DIR/server/bedrock-server
 
     # Clean up the HTML file after extracting
-    rm -f temp/version.html
+    rm -f $SCRIPT_DIR/temp/version.html
 
     # Clean up the zip file after extracting
-    rm -f temp/$LATEST_VERSION
-
-    # Move the installed server files into the correct location
-    mv  temp/bedrock-server server
+    rm -f $SCRIPT_DIR/temp/$LATEST_VERSION
 
     # Update the version.txt file to the maintain version info for future updates
     echo "Updating the version.txt file..."
@@ -45,7 +42,7 @@ mcbeinstall() {
 
     # Modify the server.properties file
     echo "Modifying server.properties file..."
-    SERVER_PROPERTIES="server/bedrock-server/server.properties"
+    SERVER_PROPERTIES="$SCRIPT_DIR/server/bedrock-server/server.properties"
 
     if [[ -f "$SERVER_PROPERTIES" ]]; then
         # Add your custom properties that needs to be enabled as default
@@ -58,16 +55,14 @@ mcbeinstall() {
     fi
 
     echo "Server install and configuration complete!"
+    echo "A trial server will run, Exit by typing stop and then enter."
 
-    echo "Server install complete!"
-
-    cd server/bedrock-server
+    # Run the SERVER
+    cd $SCRIPT_DIR/server/bedrock-server
     LD_LIBRARY_PATH=. ./bedrock_server 
-    cd ..
-    cd ..
 
-    mv server/bedrock-server/worlds/"Bedrock Level" base
+    mv $SCRIPT_DIR/server/bedrock-server/worlds/"Bedrock Level" $SCRIPT_DIR/server/bedrock-server/worlds/trial
 
-    # Back to the main script
-    mcbe
+    echo "Installation Completed, Rexecute the command to start the actual server."
+    exit 0
 }
