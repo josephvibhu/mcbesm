e25c8f6a8e7a4711e47485bd311f5b36662a5490
Joseph Vibhu <123543750+josephvibhu@users.noreply.github.com>
2024-12-26 15:12:00 +0530

    Update mcbe.sh

--- mcbesmv9.3/mcbe.sh
+++ mcbesmv9.3/mcbe.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# mcbesmv9.3 by josephvibhu
+#v9.3 by josephvibhu
 
 # Path variables
 SOURCE_DIR="/home/sudojoe/mcbesm"
@@ -7,13 +7,13 @@ SERVER_DIR="/home/sudojoe/server/bedrock-server"
 TEMP_DIR="/home/sudojoe/temp"
 
 # Source the modular functions using the relative path
-source "$SCRIPT_DIR/mcbesm/src/mcbeup.sh"
-source "$SCRIPT_DIR/mcbesm/src/mcbebak.sh"
-source "$SCRIPT_DIR/mcbesm/src/mcbeinstall.sh"
+source "$SOURCE_DIR/src/mcbeup.sh"
+source "$SOURCE_DIR/src/mcbebak.sh"
+source "$SOURCE_DIR/src/mcbeinstall.sh"
 
 mcbe() {
     # Check if the server directory exists
-    if [ -d $SCRIPT_DIR/mcbesm/server/bedrock-server ]; then
+    if [ -d $SOURCE_DIR/server/bedrock-server ]; then
         echo "Directory found! Proceeding with the script..."
     else
         # Installs Minecraft if the directory does not exist
@@ -24,7 +24,7 @@ mcbe() {
     
     # Listing Worlds
     echo "Listing available worlds in 'server/bedrock-server/worlds/' directory..."
-    worlds=$(find $SCRIPT_DIR/mcbesm/server/bedrock-server/worlds/ -mindepth 1 -maxdepth 1 -type d | sed 's|^.*/||')
+    worlds=$(find $SOURCE_DIR/server/bedrock-server/worlds/ -mindepth 1 -maxdepth 1 -type d | sed 's|^.*/||')
 
     [ -n "$worlds" ] || { echo "No worlds available in the 'worlds' directory!"; exit 1; }
 
@@ -36,7 +36,7 @@ mcbe() {
     world_name=$(echo "$world_name" | xargs)
 
     # Update server.properties
-    sed -i "s/^level-name=.*/level-name=$world_name/" mcbesm/server/bedrock-server/server.properties || { echo "Failed to update server.properties"; exit 1; }
+    sed -i "s/^level-name=.*/level-name=$world_name/" $SERVER_DIR/server.properties || { echo "Failed to update server.properties"; exit 1; }
 
     # Start the server
     run
@@ -45,7 +45,7 @@ mcbe() {
 
 run() {
     # Starts the server
-    cd mcbesm/server/bedrock-server
+    cd/server/bedrock-server
     echo "Starting the Minecraft server with world '$world_name'..."
     LD_LIBRARY_PATH=. ./bedrock_server
 
@@ -70,9 +70,6 @@ run() {
 # Main function
 main() {
     mcbe
-}
-
-maincbe
 }
 
 main
